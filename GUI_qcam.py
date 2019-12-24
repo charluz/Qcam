ec38069db8c071964269d47a5be3024334134e9e
charlesCY <wuchangyu@gmail.com>
2019-12-24 14:48:47 +0800

    update

--- GUI_qcam.py
+++ GUI_qcam.py
@@ -27,7 +27,8 @@ from cy_Utils.cy_TimeStamp import TimeStamp
 ###########################################################
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", '--host', type=str, default='10.34.149.29',
-	help='The host machine: localhost or IP of remote machine')
+	help='The host machine: localhost or IP of remote machine, \
+			or local:xxxx.jpg to use local test image file.')
 parser.add_argument("-p", '--port', type=int, default=-1,
 	help='The port on which to connect the host')
 parser.add_argument("-j", '--jpg', type=str, default='test001.jpg',
@@ -144,16 +145,30 @@ def implant_frame_roi(frame, roi_img, Vt, Vb):
 	# return roi_img
 
 def focusing_scoring(frame, roi_rects):
+	Debug = False
 	roi_scores = []
 	vertices = roi_rects.get_vertex_all()
+
+	if Debug:
+		cv2.imwrite("frame.jpg", frame)
+
+	idx = 0
 	for vtx in vertices:
 		roi_score = []
 		# print("Vertex: {}, {}-{}".format(vtx[0], vtx[1], vtx[2]))
 		Vt, Vb = vtx[1], vtx[2]
 
+		if Debug:
+			print("Vt= {}, Vb= {}".format(Vt, Vb))
+
 		roi_img = crop_frame_roi(frame, Vt, Vb)
 		score, lap_img = calcualte_score(roi_img)
 
+		if Debug: #-- DEBUG ONLY
+			cv2.imwrite("roi_{}.jpg".format(idx), roi_img)
+			cv2.imwrite("lap_{}.jpg".format(idx), lap_img)
+
+		idx += 1
 		roi_score.append(vtx[0]) #-- store roi name
 		roi_score.append(score/100.0)	 # store roi's focusing score
 		roi_scores.append(roi_score)
@@ -209,33 +224,50 @@ def onClose():
 evAckClose = threading.Event()
 evAckClose.clear()
 
+szlocal = 'local:'
+localImage = ""
+useLocalImage = True if args.host[:len(szlocal)] == szlocal else False
+if useLocalImage:
+	localImage = args.host[len(szlocal):]
+
 #----------------------------------------------------
 # MainGUI Initialization
 #----------------------------------------------------
-# url = 'http://192.168.7.1/frame.jpeg'
-url = "http://{}".format(args.host)
-if args.port > 0:
-	url += ":{}".format(args.port)
-url += "/{}".format(args.jpg)
 
-mainGUI = MainGUI(url)
+if not useLocalImage:
+	# url = 'http://192.168.7.1/frame.jpeg'
+	url = "http://{}".format(args.host)
+	if args.port > 0:
+		url += ":{}".format(args.port)
+	url += "/{}".format(args.jpg)
+
+mainGUI = MainGUI(localImage if useLocalImage else url)
 mainGUI.thread.start()
 time.sleep(0.01)
 
 mainGUI.root.wm_protocol("WM_DELETE_WINDOW", onClose)
 
-TS = TimeStamp(name="QCam", enable=False)
+TS = TimeStamp(name="QCam", enable=False, csv="tmlog_qcam.csv")
 TS.ProcStart()
 
 #----------------------------------------------------
 # 讀取影像初始參數
 #----------------------------------------------------
-print("Connecting {} for initial configuration...".format(mainGUI.url))
-frame_ww, frame_hh = -1, -1
-succ, frame_img = httpGet_jpeg(mainGUI.url)
-if not succ:
-	raise RuntimeError("Failed capturing jpeg from: {}".fomat(mainGUI.url))
-	exit()
+if useLocalImage:
+	print("Reading {} for initial configuration...".format(localImage))
+	frame_ww, frame_hh = -1, -1
+	frame_img = cv2.imread(localImage)
+	if frame_img is None:
+		raise RuntimeError("Failed reading image from: {}".fomat(localImage))
+		exit()
+else:
+	print("Connecting {} for initial configuration...".format(mainGUI.url))
+	frame_ww, frame_hh = -1, -1
+	succ, frame_img = httpGet_jpeg(mainGUI.url)
+	if not succ:
+		raise RuntimeError("Failed capturing jpeg from: {}".fomat(mainGUI.url))
+		exit()
+
 frame_hh, frame_ww = frame_img.shape[:2]
 frame_Vt = (0, 0)		#-- image 左上頂點
 frame_Vb = (frame_ww-1, frame_hh-1)	#-- image 右下頂點
@@ -291,24 +323,30 @@ while True:
 		print("-x-", end="")
 		continue
 
-	TS.SubStart()
-	#----------------------------------------------
-	# 向 server 提取影像
-	#----------------------------------------------
-	resp = req.get(mainGUI.url, allow_redirects=True)
-	if resp.status_code != 200:
-		print("-p-", end="")
-		continue
-	TS.SubEnd("getIMG")
 
-	TS.SubStart()
-	# print("Type - content: ", type(resp.content))
-	img_array = np.frombuffer(resp.content, dtype=np.dtype('uint8'))
-	img = cv2.imdecode(img_array, flags=cv2.IMREAD_UNCHANGED)
-	TS.SubEnd("cvtJPG")
+	if useLocalImage:
+		img = frame_img.copy()
+	else:
+		#----------------------------------------------
+		# 向 server 提取影像
+		#----------------------------------------------
+		TS.SubStart()
+		resp = req.get(mainGUI.url, allow_redirects=True)
+		if resp.status_code != 200:
+			print("-p-", end="")
+			continue
+		TS.SubEnd("getIMG")
+
+		TS.SubStart()
+		# print("Type - content: ", type(resp.content))
+		img_array = np.frombuffer(resp.content, dtype=np.dtype('uint8'))
+		img = cv2.imdecode(img_array, flags=cv2.IMREAD_UNCHANGED)
+		TS.SubEnd("cvtJPG")
 
 	#--- 計算調焦分數
+	TS.SubStart()
 	scores = focusing_scoring(img, roi_rects)
+	TS.SubEnd("scoring")
 
 	#--- 畫框 and 分數
 	draw_focusing(img, roi_rects, scores)
@@ -322,6 +360,6 @@ while True:
 		frame_index += 1
 	pass #-- end of if mainGUI.connected == True
 
-	# break
+	#break
 
 cv2.destroyAllWindows()
